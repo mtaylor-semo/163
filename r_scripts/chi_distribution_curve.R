d2a720abd75c1bb3f02551e5540f75ffdac61bf4
Mike Taylor <mtaylor@semo.edu>
2017-04-17 15:14:58 -0500

    Plot the chi-squared curve using the Curve function instead of the Density function. The curve now stops at 0 instead of trailing into negative numbers.

--- r_scripts/chi_distribution_curve.R
+++ r_scripts/chi_distribution_curve.R
@@ -2,38 +2,35 @@ df <- 3
 
 alpha_significance<-0.05
 
-# crit.chi <- round(qchisq(1-alpha_significance/2, df),2)
-
 crit.chi <- round(qchisq(1-alpha_significance, df), 2)
 
-
 x <- rchisq(100000,df)
-
-png(file='13_chi_distrib.png', bg="transparent", width=1000, height=607)
 
 plot_x_upper_lim <- 12
 
 op <- par(family='serif')
 
+png(file='13_chi_distrib.png', bg="transparent", width=1000, height=607)
+
 coord.x2 <- c(crit.chi, seq(crit.chi,plot_x_upper_lim,0.01), plot_x_upper_lim)
 coord.y2 <- c(0,dchisq(seq(crit.chi,plot_x_upper_lim,0.01),df),0)
-# coord.x1 <- c(-3, seq(-1,-crit.chi,0.01), -crit.chi)
-# coord.y1 <- c(0,dchisq(seq(-1,-crit.chi,0.01),df),0)
 plot((density(x, adjust=2)),type="n",ylim=c(0,0.25), xlim=c(0,plot_x_upper_lim), bty='l', las = 1,
-     main="", ylab="Sample Probability", xlab=bquote(chi^2 ~ "value"))
+     main="", ylab="Sample Probability", xlab=bquote(chi^2 ~ "value"), yaxs = "i")
 polygon(coord.x2,coord.y2, col='gray', border=NA)
-# polygon(coord.x1,coord.y1, col='gray', border=NA)
 
 segments(crit.chi,0, crit.chi,0.022, lty=1, col='grey60')
-text(crit.chi,0.05,bquote(chi^2==.(crit.chi)*',' ~ alpha==0.05), pos=4)
+curve(dchisq(x, df), add=TRUE)
 
-# segments(-crit.chi,0, -crit.chi,0.024, lty=1, col='grey60')
-# text(-crit.chi,0.05,bquote(chi==.(-crit.chi)), pos=2)
+text(crit.chi,0.1,bquote(chi^2==.(crit.chi)*',' ~ alpha==0.05), pos=4)
 
-lines(density(x, adjust=2), lwd=2)
+text(1,0.07, "Does not differ \nfrom expected values.", pos=4)
+text(9, 0.04, "Differs significantly \nfrom expected values.", pos=4)
+arrows(8.6, 0.0075, 9,0.04, code=1, length=0.1)
 
 
 par(op)
 
 dev.off()
+
+
 
